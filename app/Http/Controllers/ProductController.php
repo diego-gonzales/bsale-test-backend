4ca11cecf711223aca30ddf362fe7243f622e4aa
Diego Gonzales <dgonzalesj14@unc.edu.pe>
2021-08-07 18:40:48 -0500

    Se creó el API para bsale test

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = new Product;
+
+        if (request()->has('name')) {
+            $products = $products->where('name', 'LIKE', '%' . request('name') . '%');
+        }
+        if (request()->has('sort')) {
+            $products = $products->orderBy('name', request('sort'));
+        }
+
+        $products = $products->paginate()->appends([
+            'name' => request('name'),
+            'sort' => request('sort')
+        ]);
+
+        return response($products);
+    }
+
+    public function category(Category $category) {
+        $products = Product::where('category', $category->id)->get();
+        return response($products);
+        // return response($category->products()->get()); // Option 2
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        $producto = Product::findOrFail($product->id);
+        return response($producto);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        // $product = Product::findOrFail($product)->update($request->all());
+        // return response($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        // Product::destroy($product);
+        // return response('Product has been deleted');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('products', ProductController::class);
+Route::get('category/{category}', [ProductController::class, 'category']);
+
+Route::apiResource('categories', CategoryController::class);
